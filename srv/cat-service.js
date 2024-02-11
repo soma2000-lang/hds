4993c6c04d8dfe211dc86365dc3bb460236dceeb
soma2000-lang <bishnupadamajumder@gmail.com>
2024-02-11 13:45:01 +0000

    add

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -7,10 +7,10 @@ class EmployeeService extends cds.ApplicationService {
     init() {
        this.on(["READ"], "User", employeereporthandler.usersReport);
        
-        this.on('fetchEmpDetails', employeereporthandler.usersReport);
+        this.on('fetchEmpDetails', employeereporthandler.fetchEmpDetailInfo);
         // this.on('usersPhoto', employeereporthandler.fetchPhoto);
         // this.on('userjobinfo', employeereporthandler.getUsers);
- 
+        this.on('userpersonalinfo', employeereporthandler.usersReport);
         // this.on('compensationinfo', employeereporthandler.fetchSalary);
        
         return super.init();
